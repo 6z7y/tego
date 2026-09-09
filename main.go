ad9b91f5e92362ad12327b5ee39456efe0475005
6z7y <eenanho24n@proton.me>
2026-09-09 15:29:12 +0300

    support mmap

--- main.go
+++ main.go
@@ -3,20 +3,37 @@ package main
 import (
 	"fmt"
 	"os"
-	// "strings"
+	"strings"
+	"strconv"
+	// "path/filepath"
+	// "sync"
+	// "time"
 	"github.com/andelf/go-curl"
+	"github.com/edsrzf/mmap-go"
 )
 type DL_CFG struct {
 	per_thread  int
 	// ststic path output
 }
 
+type Chunk struct {
+	index		int
+	start		int64
+	end			int64
+	downloaded	int64
+	status		string
+	// file		*os.File
+	err			error
+}
+
 type DL_DATA struct {
 	url			string
 	name		string
 	file		*os.File
-	file_err    error
+	mmap		mmap.MMap
+	size	    int64
 	cfg			DL_CFG
+	chunk  []*Chunk
 }
 
 var unit = [6]string{"B", "KB", "MB", "GB", "TB", "PB"}
@@ -47,6 +64,110 @@ func progress_fn(dltotal, dlnow, ultotal, ulnow float64, userdata interface{}) b
 	return true
 }
 
+// take size of file
+func getSizeUrl(url string) (int64, error) {
+	var size int64
+	// init curl
+	easy := curl.EasyInit()
+	if easy == nil {
+		return 0, fmt.Errorf("can't init curl")
+		
+	}
+
+	defer easy.Cleanup()
+
+	easy.Setopt(curl.OPT_URL, url) // url set
+	easy.Setopt(curl.OPT_NOBODY, true) // without body information
+	easy.Setopt(curl.OPT_FOLLOWLOCATION, true) // can follow source of link for real get data
+	easy.Setopt(curl.OPT_WRITEFUNCTION, func(content []byte, userdata interface{}) bool { // discard the actual response body
+		return true
+	})
+
+	easy.Setopt(curl.OPT_HEADERFUNCTION, func(data []byte, userdata interface{}) bool { // only take content-lenght value
+		header := strings.TrimSpace(string(data))
+		if strings.HasPrefix(strings.ToLower(header), "content-length:") {
+			parts := strings.SplitN(header, ":", 2)
+			if len(parts) == 2 {
+				sizeStr := strings.TrimSpace(parts[1])
+				if val, err := strconv.ParseInt(sizeStr, 10, 64); err == nil {
+					if ptr, ok := userdata.(*int64); ok {
+						*ptr = val
+					}
+				}
+			}
+		}
+		return true
+	})
+
+	easy.Setopt(curl.OPT_WRITEHEADER, &size)
+
+	err_easy := easy.Perform()
+	if err_easy != nil {
+		return 0, err_easy
+	}
+
+	return size, nil
+}
+
+func single_dl(dl *DL_DATA) error {
+	// init a file
+	f, err := os.Create(dl.name)
+	if err != nil {
+		return fmt.Errorf("can't create file %v", err)
+	}
+	dl.file = f
+	defer dl.file.Close()
+
+	// pre size empty blocks
+	err = dl.file.Truncate(dl.size)
+	if err != nil {
+		return fmt.Errorf("can't pre allocate space: %v", err)
+	}
+
+
+	// link with mmap
+	mmap, err := mmap.Map(dl.file, mmap.RDWR, 0)
+	if err != nil {
+		return fmt.Errorf("can't memory-map file: %v", err)
+	}
+
+	dl.mmap = mmap
+	defer dl.mmap.Unmap()
+
+
+	// init a curl
+	easy := curl.EasyInit()
+	if easy == nil {
+		return fmt.Errorf("can't init curl")
+	}
+
+	defer easy.Cleanup()
+
+	var offset int64 = 0
+
+	easy.Setopt(curl.OPT_URL, dl.url) // link
+
+	// write on a file using mmap
+	easy.Setopt(curl.OPT_WRITEFUNCTION, func(data []byte, userdata interface{}) bool {
+		dlPtr := userdata.(*DL_DATA)
+		copy(dlPtr.mmap[offset:offset+int64(len(data))], data)
+		offset += int64(len(data))
+		return true
+	})
+
+	easy.Setopt(curl.OPT_WRITEDATA, dl)
+
+	// progress
+	easy.Setopt(curl.OPT_NOPROGRESS, false)
+	easy.Setopt(curl.OPT_PROGRESSFUNCTION, progress_fn)
+
+	err_easy := easy.Perform()
+	if err_easy != nil {
+		return fmt.Errorf("falied download: %v", err_easy)
+	}
+	return dl.mmap.Flush()
+}
+
 func main() {
 	// 1. arg checker
 	if len(os.Args) < 2 {
@@ -66,44 +187,17 @@ func main() {
 
 	load_cfg(&dl.cfg)
 
-	// init file
-	dl.file, dl.file_err = os.Create(dl.name)
-	if dl.file_err != nil {
-		fmt.Errorf("can't create file %v", dl.file_err)
-		return
-
-	}
-	defer dl.file.Close()
-
 	// hide cursor
 	fmt.Printf("%s", HIDE_CURSOR)
 	defer fmt.Printf("%s", SHOW_CURSOR)
 
-	// init curl
-	easy := curl.EasyInit()
-	if easy == nil {
-		fmt.Errorf("can't init curl")
-		return
-	}
+	// get size of a file
+    size, err := getSizeUrl(os.Args[1])
+    if err != nil {
+        fmt.Printf("Error: %v\n", err)
+        return
+    }
+	dl.size = size
 
-	defer easy.Cleanup()
-
-	easy.Setopt(curl.OPT_URL, dl.url) // link
-
-	// write on a file
-	easy.Setopt(curl.OPT_WRITEDATA, dl.file)
-	easy.Setopt(curl.OPT_WRITEFUNCTION, func(data []byte, userdata interface{}) bool {
-		dl.file.Write(data)
-		return true
-	})
-
-	// progress
-	easy.Setopt(curl.OPT_NOPROGRESS, false)
-	easy.Setopt(curl.OPT_PROGRESSFUNCTION, progress_fn)
-
-	err_easy := easy.Perform()
-	if err_easy != nil {
-		fmt.Errorf("falied download: %v", err_easy)
-		return
-	}
+	single_dl(&dl)
 }
