d513daa32a448f096d23502e8e41e16af866970e
6z7y <eenanho24n@proton.me>
2026-09-09 12:05:52 +0300

    support progress

--- main.go
+++ main.go
@@ -19,6 +19,33 @@ type DL_DATA struct {
 	cfg			DL_CFG
 }
 
+var unit = [6]string{"B", "KB", "MB", "GB", "TB", "PB"}
+
+func progress_fn(dltotal, dlnow, ultotal, ulnow float64, userdata interface{}) bool {
+	if dltotal > 0 {
+		percent := (dlnow / dltotal) * 100
+
+		ind_now := 0
+		ind_total := 0
+
+		b_now := dlnow
+		b_total := dltotal
+
+		for b_now >= 1024 && ind_now < 5 {
+			b_now /= 1024
+			ind_now++;
+		}
+
+		for b_total >= 1024 && ind_total < 5 {
+			b_total /= 1024
+			ind_total++;
+		}
+
+		fmt.Printf("\r[%.2f%%] %.2f %s / %.2f %s         ", percent, b_now, unit[ind_now], b_total, unit[ind_total])
+
+	}
+	return true
+}
 
 func main() {
 	// 1. arg checker
@@ -48,6 +75,10 @@ func main() {
 	}
 	defer dl.file.Close()
 
+	// hide cursor
+	fmt.Printf("%s", HIDE_CURSOR)
+	defer fmt.Printf("%s", SHOW_CURSOR)
+
 	// init curl
 	easy := curl.EasyInit()
 	if easy == nil {
@@ -57,12 +88,18 @@ func main() {
 
 	defer easy.Cleanup()
 
-	easy.Setopt(curl.OPT_URL, dl.url)
+	easy.Setopt(curl.OPT_URL, dl.url) // link
+
+	// write on a file
 	easy.Setopt(curl.OPT_WRITEDATA, dl.file)
 	easy.Setopt(curl.OPT_WRITEFUNCTION, func(data []byte, userdata interface{}) bool {
 		dl.file.Write(data)
 		return true
 	})
+
+	// progress
+	easy.Setopt(curl.OPT_NOPROGRESS, false)
+	easy.Setopt(curl.OPT_PROGRESSFUNCTION, progress_fn)
 
 	err_easy := easy.Perform()
 	if err_easy != nil {
